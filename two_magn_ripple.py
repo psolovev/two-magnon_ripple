670e97d8709886f1bef7df07591cf2031d74fa7f
Platon Solovev <platon.solovev@gmail.com>
2019-09-05 18:38:10 +0700

    plot uniform FMR and spin wave cross point

--- two_magn_ripple.py
+++ two_magn_ripple.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.size'] = 14
+plt.rcParams["font.family"] = "Times New Roman"
+
 params = {'Ms': 955,
           'gamma': 2.95,
           'A': 10**-6}
@@ -30,6 +33,18 @@ def spin_wave_fmr(H, params, d):
     return omega_k
 
 
+def find_cross(omega, omega_k, H):
+    diff = np.abs(omega - omega_k)
+    min_diff_ix = np.argmin(diff)
+    toler = abs(omega[min_diff_ix] - omega[min_diff_ix-1])
+
+    if min_diff_ix > 0 and diff[min_diff_ix] < toler:
+        cross_y = omega_k[min_diff_ix]
+        cross_x = H[min_diff_ix]
+        return cross_x, cross_y
+    else:
+        return False, False
+
 
 def plot_fmr(params):
     H = np.arange(1, 400, 1)
@@ -42,10 +57,18 @@ def plot_fmr(params):
 
     for d in d_vect:
         omega_k = spin_wave_fmr(H, params, d)
+        cross_x, cross_y = find_cross(omega, omega_k, H)
         plt.plot(H, omega_k * 10**-3, label='d = ' + str(int(np.ceil(d*10**7))) + ' nm')
+        if cross_x:
+            plt.plot(cross_x, cross_y * 10**-3, '*r', markersize=8)
     plt.xlabel('H (Oe)')
     plt.ylabel('Frequency (GHz)')
-    plt.legend(loc='lower right')
+
+    legfont = {'fontsize': 10}
+    plt.legend(loc='lower right', **legfont)
+    plt.savefig('freq_vs_field.png', bbox_inches='tight', dpi=600)
+
+
     plt.show()
 
 
